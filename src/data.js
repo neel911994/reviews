d692e6e2c9ff56a70d421a18d98ed48aa7fb1e97
neelam shinde <neelam.shinde@oracle.com>
2020-12-31 13:08:48 +0530

    review changes

--- src/data.js
+++ src/data.js
@@ -6,7 +6,7 @@ const reviews = [
     image:
       'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
     text:
-      "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
+      "Well, Amazon is a great place to work, with a nice salary, According to the job. There are lots of opportunities for your growth, you can switch inbetween various roles, giving you the best opportunity to find your field.. salary, hikes and other incentives are more than any other companies. But, Tron is somewhat stressful job and working in shifts makes it worse. Stress on the eyes and the mind, are two major things, which one should take care of while doing this job.",
   },
   {
     id: 2,
@@ -15,7 +15,7 @@ const reviews = [
     image:
       'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg',
     text:
-      'Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.',
+      'Was very good experience working with Amazon as I was in pilot process for women work from home in my state. Was new experience to handle customer directly as I was working for email process. As women it was easy to handle home and job.',
   },
   {
     id: 3,
@@ -24,7 +24,7 @@ const reviews = [
     image:
       'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
     text:
-      'Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.',
+      'Amazon has great office culture and all of the higher authorities are best in there roles.Perks and befinits are also great.You will be trained and flipped to each and every skill which will help you to improve your knowledge.',
   },
   {
     id: 4,
@@ -33,7 +33,7 @@ const reviews = [
     image:
       'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg',
     text:
-      'Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ',
+      'Excellent company to work and job environment is totally good and all other managers were supportive and friendly and incentives overtime as per decision to work or not..... like this many facitlites',
   },
 ];
 
